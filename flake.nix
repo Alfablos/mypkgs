28d9d7f176ac9d1a758e97c0379b08a969ea2d89
Antonio Galanti <antonio.galanti@gmail.com>
2025-02-08 13:45:11 +0100

    trying to build tabby 0.23.0

--- flake.nix
+++ flake.nix
@@ -15,11 +15,24 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       in
       {
+        packages.default = self.packages.${system}.tabby;
+
         packages.posting = pkgs.callPackage ./posting.nix { };
-        packages.default = self.packages.${system}.posting;
+        
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ta/tabby/package.nix
+        packages.tabby = with pkgs; callPackage ./tabby {
+          cudaSupport = true;
+          acceleration = "cuda";
+          # nvidiaPackage = linuxPackages.nvidia_x11_latest;
+          stdenv = gcc11Stdenv;
+          pkg-config = pkg-config.override {
+            stdenvNoCC = gcc11Stdenv;
+          };
+          openssl = openssl.override { stdenv = gcc11Stdenv; };
+        };
       }
     );
 }
